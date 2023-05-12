b6780f4a591859f15c9a3de8b2e615e291e0c6d1
marcel <marcelinocost69@gmail.com>
2023-05-12 19:11:06 -0300

    commit - 03

--- js/storage.js
+++ js/storage.js
@@ -1,4 +1,4 @@
-const criaStorage = () => {
+/* const criaStorage = () => {
 
   const dadosDoVideo = {
     "titulo": `Armazenando dados no localStorage em JS`,
@@ -43,6 +43,62 @@ const clearStorage = () => {
 // armazenar dados mais complexos no Storage
 
 document.getElementById('cria-storage').onclick = criaStorage
+document.getElementById('remove-storage').onclick = removeStorage
+document.getElementById('limpar-storage').onclick = clearStorage
+
+getStorage() */  
+
+
+
+const produtos = () => {
+
+  const dadosDoProduto = {
+    "nome": `Caneta Bic`,
+    "preco": 1.90,
+    "desconto": 0.05,
+     "caracteristicas": [
+      {nome: 'Cor', valor: 'preco'},
+      {nome: 'Material', valor: 'Plástico'}
+     ]
+  }
+
+  localStorage.setItem(`produto`, JSON.stringify(dadosDoProduto))
+}
+
+const getStorage = () => {
+  const dadosDoProduto = localStorage.getItem('produto')
+
+  if(dadosDoProduto) {
+    const objDadosDoProduto = JSON.parse(dadosDoProduto)
+    const {nome, preco, desconto} = objDadosDoProduto
+
+    const resumo = nome + '<br>' + preco + '<br>' + desconto
+    
+    document.querySelector('#conteudo1').innerHTML = resumo
+  }
+  else if(!dadosDoProduto) {
+    document.querySelector('#conteudo2').innerHTML = 'A chave não existe!'      
+  } 
+}  
+
+const removeStorage = () => {
+  localStorage.removeItem('produto')
+}
+
+const clearStorage = () => {
+  localStorage.clear()
+}
+
+//== Méthods ==
+// setItem
+// getItem
+// removeItem
+// clear
+// Extra:
+// Utilize os JSON.stringfy e JSON.parse para poder 
+// armazenar dados mais complexos no Storage
+
+document.getElementById('cria-storage').onclick = produtos
 document.getElementById('remove-storage').onclick = removeStorage
 document.getElementById('limpar-storage').onclick = clearStorage
 
